fd8325e389d412173c8584388b4cd2d9209a3908
hemge <hemge@example.com>
2026-03-08 14:51:21 +0100

    Bump version to 0.2.9 with versionCode 100 to fix rollout conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "co.dynag.scrybook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.2.8"
+        versionCode = 100
+        versionName = "0.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
