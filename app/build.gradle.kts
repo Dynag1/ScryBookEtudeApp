26581bbbf6f345858f1a2d7e8bd711b8d1e16002
hemge <hemge@example.com>
2026-03-15 18:26:01 +0100

    Update version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         }
 
         versionCode = commitCount
-        versionName = "0.0.3"
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
